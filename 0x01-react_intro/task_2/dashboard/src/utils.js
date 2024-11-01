af4009c2c2d37ea20c4c63c724fe0cc5279be51b
FarieB <faraibande@gmail.com>
2024-11-01 23:18:21 +0000

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,9 +1,5 @@
-// task_1/dashboard/src/utils.js
+/* full-year */
+export const getFullYear = () => new Date().getFullYear();
 
-export function getFullYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? "Holberton School" : "Holberton School main dashboard";
-}
+/* footer-copy */
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
