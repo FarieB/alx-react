89604eb8ad2ed7b63849b1d615667e0e5d147050
FarieB <133366471+FarieB@users.noreply.github.com>
2024-11-12 14:47:52 +0200

    Update NotificationItem.test.js

--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -3,21 +3,29 @@ import { shallow } from 'enzyme';
 import NotificationItem from './NotificationItem';
 
 describe("Testing <NotificationItem />", () => {
-  let  wrapper;
+  let wrapper;
 
   it("<NotificationItem /> renders without crashing", () => {
     wrapper = shallow(<NotificationItem />);
-    expect(wrapper.exists());
+    expect(wrapper.exists()).toBe(true);
   });
 
-  it("<NotificationItem />  renders the correct html by passing dummy type and value props,", () => {
+  it("<NotificationItem /> renders the correct html by passing dummy type and value props", () => {
     wrapper = shallow(<NotificationItem type="default" value="test" />);
     expect(wrapper.find("li").text()).toBe("test");
     expect(wrapper.find("li").prop("data-notification-type")).toBe("default");
+
+    // Check if the 'default' class (styling) is applied correctly
+    expect(wrapper.find("li").hasClass('default')).toBe(true);
   });
 
-  it("<NotificationItem />  renders the correct html by passing a dummy html prop,", () => {
-    wrapper = shallow(<NotificationItem html={{__html:"<u>test</u>"}} />);
-    expect(wrapper.find("li").html()).toBe("<li data-notification-type=\"default\"><u>test</u></li>");
+  it("<NotificationItem /> renders the correct html by passing a dummy html prop", () => {
+    wrapper = shallow(<NotificationItem html={{__html: "<u>test</u>"}} />);
+    expect(wrapper.find("li").html()).toBe('<li data-notification-type="default"><u>test</u></li>');
+  });
+
+  it("<NotificationItem /> applies correct styles for 'urgent' type", () => {
+    wrapper = shallow(<NotificationItem type="urgent" value="urgent test" />);
+    expect(wrapper.find("li").hasClass('urgent')).toBe(true);
   });
 });
