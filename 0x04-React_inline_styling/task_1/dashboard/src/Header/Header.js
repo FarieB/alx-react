6fd91fb4fa017dd55287b19cea3a106274c4cdd2
FarieB <133366471+FarieB@users.noreply.github.com>
2024-11-12 14:11:17 +0200

    Update Header.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import logo from '../assets/holberton_logo.jpg';
-import './Header.css';
 
 function Header() {
   return (
-    <div className="App-header">
-      <img src={logo} alt="logo" />
-      <h1>School dashboard</h1>
+    <div className={css(styles.appHeader)}>
+      <img src={logo} alt="logo" className={css(styles.logo)} />
+      <h1 className={css(styles.title)}>School dashboard</h1>
     </div>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  appHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    borderBottom: '3px solid #e1484c',
+    padding: '20px',
+  },
+  logo: {
+    height: '100px',
+    marginRight: '20px',
+  },
+  title: {
+    fontSize: '2.5em',
+    color: '#e1484c',
+  },
+});
 
 export default Header;
