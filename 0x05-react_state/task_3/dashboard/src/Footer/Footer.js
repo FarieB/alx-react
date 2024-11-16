482892dd9ed225c7660302c1a8475fe5d77a9c8e
FarieB <133366471+FarieB@users.noreply.github.com>
2024-11-16 19:34:04 +0200

    3. Context consumer & advanced state

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFooterCopy, getFullYear } from '../utils/utils';
+import { AppContext } from '../AppContext'; // Import the AppContext
 
 function Footer() {
+  const { user } = useContext(AppContext); // Get the user object from context
+
   return (
     <div className="App-footer">
       <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      {user.isLoggedIn && user.email && (
+        <p>
+          <a href="/contact">Contact us</a>
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
 export default Footer;
