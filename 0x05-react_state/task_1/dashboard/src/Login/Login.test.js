e361c77e46f7d76ced902e1a09eac2ad2d34b2a3
FarieB <133366471+FarieB@users.noreply.github.com>
2024-11-16 18:52:38 +0200

    1. Controlled components and state callback

--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -6,18 +6,42 @@ import { StyleSheetTestUtils } from "aphrodite";
 beforeEach(() => {
   StyleSheetTestUtils.suppressStyleInjection();
 });
+
 afterEach(() => {
   StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 });
 
-describe("Header", () => {
+describe("Login Component", () => {
   it("should render without crashing", () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.exists()).toEqual(true);
   });
+
   it("should have 2 input tags and 2 label tags", () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.find("label")).toHaveLength(2);
-    expect(wrapper.find("input")).toHaveLength(2);
+    expect(wrapper.find("input[type='email']")).toHaveLength(1);
+    expect(wrapper.find("input[type='password']")).toHaveLength(1);
+  });
+
+  it("should disable the submit button by default", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find("input[type='submit']").prop("disabled")).toEqual(true);
+  });
+
+  it("should enable the submit button when email and password are not empty", () => {
+    const wrapper = shallow(<Login />);
+    wrapper.find("input[type='email']").simulate("change", { target: { value: "test@test.com" } });
+    wrapper.find("input[type='password']").simulate("change", { target: { value: "password" } });
+    expect(wrapper.find("input[type='submit']").prop("disabled")).toEqual(false);
+  });
+
+  it("should call handleLoginSubmit on form submit", () => {
+    const wrapper = shallow(<Login />);
+    const handleLoginSubmitMock = jest.fn();
+    wrapper.instance().handleLoginSubmit = handleLoginSubmitMock;
+    wrapper.instance().forceUpdate(); // Ensure updated function is used
+    wrapper.find("form").simulate("submit", { preventDefault: jest.fn() });
+    expect(handleLoginSubmitMock).toHaveBeenCalled();
   });
 });
