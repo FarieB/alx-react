899669ddad958b4537d50141c63557696c984faa
FarieB <133366471+FarieB@users.noreply.github.com>
2024-11-16 19:44:13 +0200

    3. Context consumer & advanced state

--- 0x05-react_state/task_3/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_3/dashboard/src/Notifications/Notifications.js
@@ -1,32 +1,20 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { StyleSheet, css } from "aphrodite";
 import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
 
-class Notifications extends Component {
-  constructor(props) {
-    super(props);
-
-    this.markAsRead = this.markAsRead.bind(this);
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return nextProps.length > this.props.listNotifications.length;
-  }
-
-  markAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
-  }
-
+class Notifications extends PureComponent {
   render() {
+    const { displayDrawer, listNotifications, markNotificationAsRead } = this.props;
+
     return (
       <React.Fragment>
         <div className={css(styles.menuItem)}>
           <p>Your notifications</p>
         </div>
-        {this.props.displayDrawer ? (
+        {displayDrawer ? (
           <div className={css(styles.Notifications)}>
             <button
               style={{
@@ -48,23 +36,21 @@ class Notifications extends Component {
             >
               <img src={closeIcon} alt="close icon" width="10px" />
             </button>
-            {this.props.listNotifications.length !== 0 ? <p>Here is the list of notifications</p> : null}
+            {listNotifications.length !== 0 ? <p>Here is the list of notifications</p> : null}
             <ul className={css(styles.notificationList)}>
-              {this.props.listNotifications.length === 0 ? (
+              {listNotifications.length === 0 ? (
                 <NotificationItem type="default" value="No new notification for now" />
               ) : null}
-              {this.props.listNotifications.map((val, idx) => {
-                return (
-                  <NotificationItem
-                    type={val.type}
-                    value={val.value}
-                    html={val.html}
-                    key={val.id}
-                    markAsRead={this.markAsRead}
-                    id={val.id}
-                  />
-                );
-              })}
+              {listNotifications.map((val) => (
+                <NotificationItem
+                  type={val.type}
+                  value={val.value}
+                  html={val.html}
+                  key={val.id}
+                  markAsRead={markNotificationAsRead}
+                  id={val.id}
+                />
+              ))}
             </ul>
           </div>
         ) : null}
@@ -75,27 +61,22 @@ class Notifications extends Component {
 
 const styles = StyleSheet.create({
   Notifications: {
-    padding: 0, // No padding for the entire notification panel
+    padding: 0,
     border: "2px dashed red",
-    position: "fixed", // Use fixed to make it occupy the entire screen
+    position: "fixed",
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
     backgroundColor: "#fff",
-    zIndex: 999, // Ensure it is above other elements
-    fontSize: "20px", // Set the font size to 20px
+    zIndex: 999,
+    fontSize: "20px",
   },
 
   notificationList: {
-    padding: 0, // Remove padding from the ul element
-    margin: 0, // Ensure no margin on the ul
-    listStyle: "none", // Remove the default list styling
-  },
-
-  "notification-header": {
-    display: "flex",
-    justifyContent: "space-between",
+    padding: 0,
+    margin: 0,
+    listStyle: "none",
   },
 
   menuItem: {
@@ -118,11 +99,13 @@ const styles = StyleSheet.create({
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
   listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  markNotificationAsRead: PropTypes.func,
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
   listNotifications: [],
+  markNotificationAsRead: (id) => console.log(`Notification ${id} has been marked as read`),
 };
 
 export default Notifications;
